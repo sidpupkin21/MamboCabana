443e1d0d76775353f34ada32efb2920673ee59e3
Ahmed Sid Mohamed <75706407+sidpupkin21@users.noreply.github.com>
2023-06-09 17:02:40 +0400

    Home, shared header + footer, temp images

---
+++ shared/footer.php
@@ -0,0 +1,62 @@
+<!--Footer-->
+<footer class="footer-area section-padding-80-0">
+    <div class="main-footer-area">
+        <div class="container">
+            <div class="row align-items-baseline justify-content-between">
+
+                <!--Hotel Information & contact-->
+                <div class="col-12 col-sm-6 col-lg-0">
+                    <div class="single-footer-widget mb-80">
+                        <!--Footer logo goes here-->
+                        <a href="#" class="footer-logo">
+
+                            <img str="." />
+                            Footer Logo Goes here
+                        </a>
+                        <h6>+255-776-886-630</h6>
+                        <h6>+971-52-218-1823</h6>
+                        <span>admin@email.com</span>
+                        <span>Address goes here</span>
+                    </div>
+                </div>
+
+                <!--Widget Area with page links-->
+                <div class="col-5 col-sm-4 col-lg-3">
+                    <div class="single-footer-widget mb-80">
+                        <h5 class="widget-title">Links</h5>
+                        <ul class="footer-nav">
+                            <li style="list-style-type: none;"><a href="#"><iconify-icon icon="bi:caret-right"></iconify-icon>Rooms & Suites</a></li>
+                            <li style="list-style-type: none;"><a href="#"><iconify-icon icon="bi:caret-right"></iconify-icon>Activities</a></li>
+                            <li style="list-style-type: none;"><a href="#"><iconify-icon icon="bi:caret-right"></iconify-icon>Facilities</a></li>
+                            <li style="list-style-type: none;"><a href="#"><iconify-icon icon="bi:caret-right"></iconify-icon>Location & Tips</a></li>
+                            <li style="list-style-type: none;"><a href="#"><iconify-icon icon="bi:caret-right"></iconify-icon>Client Reviews</a></li>
+                            <li style="list-style-type: none;"><a href="#"><iconify-icon icon="bi:caret-right"></iconify-icon>Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!--Copyright Area-->
+                <div class="container">
+                    <div class="copywrite-content">
+                        <div class="row align-items-center">
+                            <div class="col-12">
+                                <div class="copywrite-text">
+                                    <p>&copy; Scope Management | All rights reserved</p>
+                                </div>
+                                <div class="social-info">
+                                    <!-- Add Links to socials here -->
+                                    <a href="#"><iconify-icon icon="ant-design:facebook-filled"></iconify-icon></a>
+                                    <a href="https://www.booking.com/hotel/tz/villa-fiona-marumbi.en-gb.html?aid=304142&label=gen173nr-1FCAEoggI46AdIM1gEaAKIAQGYAQm4ARfIAQzYAQHoAQH4AQyIAgGoAgO4AonIhqQGwAIB0gIkZjJkOGNhNDgtMzlhMi00MDE3LWFlZWYtYTg1YTg2NTI3ZDU42AIG4AIB&sid=bd321b716a00704b5f020c5231bc725b&all_sr_blocks=701788004_288735695_2_42_0;checkin=2023-06-27;checkout=2023-06-28;dest_id=-2568070;dest_type=city;dist=0;group_adults=2;group_children=0;hapos=1;highlighted_blocks=701788004_288735695_2_42_0;hpos=1;matching_block_id=701788004_288735695_2_42_0;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;sr_order=popularity;sr_pri_blocks=701788004_288735695_2_42_0__8500;srepoch=1686313086;srpvid=17b5567ecd8a0137;type=total;ucfs=1&#hotelTmpl"><iconify-icon icon="tabler:brand-booking"></iconify-icon></a>
+                                    <a href="#"><iconify-icon icon="mdi:instagram"></iconify-icon></a>
+                                    <a href="#"><iconify-icon icon="mdi:twitter"></iconify-icon></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</footer>
